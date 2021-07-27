4d6c6f3a14df857ddf16b012b5c64adfbf500661
nikrogalskiy <rogalskiy@gmail.com>
2021-07-28 01:27:52 +0300

    изменил методы

--- client/js/script.js
+++ client/js/script.js
@@ -2,27 +2,28 @@ class View {
 	constructor() {
 		this.citiesСurrent = document.querySelector(".cities__current");
 		this.weatherInfo = document.querySelector(".weather__info");
-		this.citiesItems = document.querySelector(".cities__items");
+		this.citiesList = document.querySelector(".cities__list");
 		this.button = document.querySelector(".btn");
 		this.input = document.querySelector(".input");
 		this.widget;
 		this.widgetCity;
 		this.widgetDetailed;
+		this.buttons;
 	}
 
 
 
 
-	createWidget(city, temp, description, icon) {
+	createWidget(obj) {
 		let currentWidget = `
 		<a href="#" class="link">
 			<h2>Ваш город</h2>
 			<span class="wraper">
-			<span class="icon"><img src="https://openweathermap.org/img/wn/${icon}.png" alt=""></span>
-			<span class="temp">${temp}<span class="deg"> C&deg</span></span>
+			<span class="icon"><img src="https://openweathermap.org/img/wn/${obj[0].weather[0].icon}.png" alt=""></span>
+			<span class="temp">${obj[0].main.temp}<span class="deg"> C&deg</span></span>
 			</span>
-			<span class="description">${description}</span>
-			<span class="current-city">${city}</span>
+			<span class="description">${obj[0].weather[0].description}</span>
+			<span class="current-city">${obj[0].name}</span>
 		</a>
 		`;
 		this.widget = currentWidget;
@@ -30,6 +31,7 @@ class View {
 
 	createWidgetCity(city, temp, description, icon) {
 		let currentWidgetCity = `
+		<li>
 		<a href="#" class="link">
 			<h2>погода в</h2>
 			<span class="wraper">
@@ -39,32 +41,39 @@ class View {
 			<span class="description">${description}</span>
 			<span class="current-city">${city}</span>
 		</a>
+		</li>
 		`;
 		this.widgetCity = currentWidgetCity;
 	}
 
-	createDetailedWeatherInformation(city, temp, description, icon, country, wind, humidity) {
+	createDetailedWeatherInformation(obj) {
 		let detailedWeatherInformation = `
 			<div class="info-weather__header">
-					<div class="info-weather__city">${city},</div>
-					<div class="info-weather country">${country}</div>
+					<div class="info-weather__city">${obj[0].name},</div>
+					<div class="info-weather country">${obj[0].sys.country}</div>
 			</div>
 			<div class="info-weather_body">
-					<div class="info-weather__icon"><img src="https://openweathermap.org/img/wn/${icon}.png" alt=""></div>
-					<div class="info-weather__temp">${temp}<span> C&deg</span></div>
+					<div class="info-weather__icon"><img src="https://openweathermap.org/img/wn/${obj[0].weather[0].icon}.png" alt=""></div>
+					<div class="info-weather__temp">${obj[0].main.temp}<span> C&deg</span></div>
 					<div class="info-weather__other">
-						<div class="info-weather__description">небо: ${description}</div>
-						<div class="info-weather__wind">ветер: ${wind} м/с</div>
-						<div class="info-weather__humiditu"><span>влажность: ${humidity} %</span></div>
+						<div class="info-weather__description">небо: ${obj[0].weather[0].description}</div>
+						<div class="info-weather__wind">ветер: ${obj[0].wind.speed} м/с</div>
+						<div class="info-weather__humiditu"><span>влажность: ${obj[0].main.humidity} %</span></div>
 					</div>
-					<!--
-					<div class="info-weather__btn">
+					<!--<div class="info-weather__btn">
 						<button>Удалить</button>
-					</div>
-					-->
+					</div>-->
 			</div>
 `;
 		this.widgetDetailed = detailedWeatherInformation;
+	}
+
+	createButtons() {
+		let btn = `
+			<div class="info-weather__btn">
+				<button>Удалить</button>
+			</div>`;
+		this.buttons = btn;
 	}
 
 }
@@ -77,6 +86,7 @@ class Model {
 			timeout: 5000,
 			maximumAge: 0
 		};
+		this.currentSity = [];
 		this.listSities = [];
 		this.inputValue;
 	}
@@ -93,22 +103,38 @@ class Model {
 		this.view.input.value = '';
 	}
 
+	getObjCurrentCity(obj) {
+		this.currentSity[0] = obj;
+	}
+
+
+	showWidget(obj) {
+		this.view.createWidget(obj)
+		this.view.citiesСurrent.innerHTML = this.view.widget;
+	}
+	showWidgetDetailed(obj) {
+		this.view.createDetailedWeatherInformation(obj)
+		this.view.weatherInfo.innerHTML = this.view.widgetDetailed;
+		this.view.createButtons();
+
+	}
+
+	showButtons() {
+		let infoWeatherBody = document.querySelector(".info-weather_body");
+		infoWeatherBody.innerHTML += this.view.buttons;
+
+	}
+
+
+
 	getShowWeaterCurrentCity() {
 		let success = async (pos) => {
 			let latitude = pos.coords.latitude;
 			let longitude = pos.coords.longitude;
 			let weatherInfo = await this.request(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&lang=ru&appid=35859dc542ddefeb9aba8b07fdb7e774`);
-			let city = weatherInfo.name;
-			let temp = weatherInfo.main.temp;
-			let description = weatherInfo.weather[0].description;
-			let icon = weatherInfo.weather[0].icon;
-			let country = weatherInfo.sys.country;
-			let wind = weatherInfo.wind.speed;
-			let humidity = weatherInfo.main.humidity;
-			this.view.createWidget(city, temp, description, icon);
-			this.view.citiesСurrent.innerHTML = this.view.widget;
-			this.view.createDetailedWeatherInformation(city, temp, description, icon, country, wind, humidity);
-			this.view.weatherInfo.innerHTML = this.view.widgetDetailed;
+			let getObjCurrentCity = await this.getObjCurrentCity(weatherInfo);
+			let showWidget = await this.showWidget(this.currentSity);
+			let showWidgetDetailed = await this.showWidgetDetailed(this.currentSity);
 		};
 		let error = (err) => {
 			console.warn(`ERROR(${err.code}): ${err.message}`);
@@ -118,17 +144,17 @@ class Model {
 	}
 
 	async getWeather(cityName) {
-		let weatherInformation = await this.request(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=ru&appid=35859dc542ddefeb9aba8b07fdb7e774`);
-		this.listSities.push(weatherInformation);
-		let city = weatherInformation.name;
-		let temp = weatherInformation.main.temp;
-		let description = weatherInformation.weather[0].description;
-		let icon = weatherInformation.weather[0].icon;
-		let country = weatherInformation.sys.country;
-		let wind = weatherInformation.wind.speed;
-		let humidity = weatherInformation.main.humidity;
-		this.view.createWidgetCity(city, temp, description, icon);
-		this.view.citiesItems.innerHTML += this.view.widgetCity;
+		// let weatherInformation = await this.request(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&lang=ru&appid=35859dc542ddefeb9aba8b07fdb7e774`);
+		// this.listSities.push(weatherInformation);
+		// let city = weatherInformation.name;
+		// let temp = weatherInformation.main.temp;
+		// let description = weatherInformation.weather[0].description;
+		// let icon = weatherInformation.weather[0].icon;
+		// let country = weatherInformation.sys.country;
+		// let wind = weatherInformation.wind.speed;
+		// let humidity = weatherInformation.main.humidity;
+		// this.view.createWidgetCity(city, temp, description, icon);
+		// this.view.citiesList.innerHTML += this.view.widgetCity;
 
 	}
 
